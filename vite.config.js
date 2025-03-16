546205157998e9d8776258352272adb4c612ea5b
alexaubin-png <157660617+alexaubin-png@users.noreply.github.com>
2025-03-16 02:58:02 -0400

    added vite config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './public', // This points Vite to the public directory
-  build: {
-    outDir: '../dist', // Set the output directory (relative to the public directory)
-  },
-});
